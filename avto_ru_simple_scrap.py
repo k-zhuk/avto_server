f6d17b9b84fe5a6cdb624b086497ad4b7f93f69b
k-zhuk <zhuk_kd@mail.ru>
2023-05-20 01:08:09 +0300

    add test file

---
+++ avto_ru_simple_scrap.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+import requests
+import os
+import shutil
+import glob
+import io
+import re
+from urllib.request import urlopen
+import seaborn as sns
+import matplotlib.pyplot as plt
+import telegram
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+# spb, geo 0 km
+avito_avto = 'https://www.avito.ru/sankt-peterburg/avtomobili?cd=1&radius=0&s=104&searchRadius=0'
+avto_ru = 'https://auto.ru/sankt-peterburg/cars/all/'
+drom = 'https://spb.drom.ru/auto/all/'
+sber_avto = 'https://sberauto.com/sankt-peterburg/cars?g' \
+            'eoDistance=0&rental_car=exclude_rental&isCreditSearchEnabled=false' \
+            '&isWarrantySearchEnabled=false&isNoMileageInRussiaEnabled=false'
+
+# add options to run in bg
+options = webdriver.ChromeOptions()
+options.add_argument('--headless=new')
+
+# get html text
+browser = webdriver.Chrome(options=options)
+browser.get(avto_ru)
+try:
+    captcha_checkbox = browser.find_element(By.ID, 'js-button')
+    captcha_checkbox.click()
+except Exception as e:
+    pass
+
+time.sleep(5)
+html_text = browser.page_source
+
+resp_list = re.findall(r'itemRadius.*Санкт\D*itemCount\D*\d{1,3}.{,10}\d{1,3}', html_text)
+
+avto_ru_total_cars = ''.join(re.findall(r'\d+', resp_list[0]))
+print('avto_ru: ', avto_ru_total_cars)
+
+browser.quit()
